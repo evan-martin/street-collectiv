16929f8d1e35f8bc2b1950877e944b2a670e15e9
Evan Martin <evanm689@gmail.com>
2020-06-25 13:00:04 -0400

    added products component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
 import DisplayStores from './components/DisplayStores'
+import DisplayProducts from './components/DisplayProducts'
 
 class App extends Component {
 
   render(){
     return(
-      < DisplayStores />
+      <BrowserRouter>
+        <Route exact path = '/' component = { DisplayStores } />
+        <Route exact path = '/products' component = { DisplayProducts } />
+      </BrowserRouter>
     );
   }
 }
