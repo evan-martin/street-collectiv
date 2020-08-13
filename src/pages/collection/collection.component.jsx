195c75daad15ce5270008e8eeb9f1938a9874137
evan-martin <evanm689@gmail.com>
2020-08-13 19:30:05 -0400

    add shop social media tab

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import {connect} from "react-redux";
 
-import {Tab} from "semantic-ui-react";
+import {Tab, Container} from "semantic-ui-react";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import {selectCollection} from "../../redux/shop/shop.selectors";
+
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import MailIcon from "@material-ui/icons/Mail";
+import {red} from "@material-ui/core/colors";
 
 import "./collection.styles.scss";
 
@@ -16,6 +22,7 @@ const CollectionPage = ({collection}) => {
     {
       menuItem: "Our Story",
       render: () => (
+        <Container maxWidth="sm">
         <Tab.Pane>
           <div className="our-story">
             <div
@@ -28,15 +35,19 @@ const CollectionPage = ({collection}) => {
               <div className="title">
                 <h1>{title}</h1>
               </div>
-              <div className="bio">{bio}</div>
+              <div className="bio">
+              <p>{bio}</p>
+              </div>
             </div>
           </div>
         </Tab.Pane>
+        </Container>
       )
     },
     {
       menuItem: "Shop",
       render: () => (
+        <Container maxWidth="sm">
         <Tab.Pane>
           <div className="collection-page">
             <h2 className="title">{title}</h2>
@@ -48,24 +59,41 @@ const CollectionPage = ({collection}) => {
           </div>
           ,
         </Tab.Pane>
+        </Container>
       )
     },
     {
       menuItem: "Find Us",
       render: () => (
+        <Container maxWidth="sm">
         <Tab.Pane>
-          <div id="map-container">
-          map is hard :(
-          </div>
+          <div id="map-container">map is hard :(</div>
         </Tab.Pane>
+        </Container>
       )
     },
     {
       menuItem: "Connect",
       render: () => (
+        <Container maxWidth="sm">
         <Tab.Pane>
-          <div id="map-container"></div>
+        <h2>Get in touch with {title}!</h2>
+          <div id="social-media">
+            <div className="social-icon">
+              <InstagramIcon color="secondary" fontSize="large" />
+            </div>
+            <div className="social-icon">
+              <TwitterIcon color="primary" fontSize="large" />
+            </div>
+            <div className="social-icon">
+              <FacebookIcon color="primary" fontSize="large" />
+            </div>
+            <div className="social-icon">
+              <MailIcon style={{color: red[500]}} fontSize="large" />
+            </div>
+          </div>
         </Tab.Pane>
+        </Container>
       )
     }
   ];
