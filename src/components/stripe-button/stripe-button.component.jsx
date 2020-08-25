a3a79681ff09f3639ed6e2333d2915c0247eccf5
evan-martin <evanm689@gmail.com>
2020-08-25 14:39:44 -0400

    pass price to stripe button and create token function

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 
-const StripeButton = ({price}) => {
- 
- 
+const StripeButton = ({ price }) => {
+  const stripePrice = price*100;
+  const publicKey = "pk_test_51HHbIrKx1GFK0jKDF4ih2zqyJobVb2RpyYXmf53LAlayVqT00qxZfgLsMw26Mb5Vyi5eV2hB0zcEQ69Xp5GoNhav00VygjfmsD";
 
-    return (
-      <StripeCheckout
-
-        stripeKey="pk_test_51HHbIrKx1GFK0jKDF4ih2zqyJobVb2RpyYXmf53LAlayVqT00qxZfgLsMw26Mb5Vyi5eV2hB0zcEQ69Xp5GoNhav00VygjfmsD"
-        name="Street Collectiv"
-        shippingAddress
-        billingAddress={false}
-        allowRememberMe={false}
-      >
-        <button className="custom-button">Checkout</button>
-      </StripeCheckout>
-    );
+  const onToken = token => {
+    console.log(token)
+    alert('Payment Successful')
   }
 
-  export default StripeButton
+  return (
+    <StripeCheckout
+      stripeKey= {publicKey}
+      name="Street Collectiv"
+      shippingAddress
+      billingAddress={false}
+      allowRememberMe={false}
+      amount={stripePrice}
+      token={onToken}
+    >
+      <button className="custom-button">Checkout</button>
+    </StripeCheckout>
+  );
+};
+
+export default StripeButton;
