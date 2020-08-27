196ac79ead9871b90c192f3bc81815bb158e4987
evan-martin <evanm689@gmail.com>
2020-08-27 12:08:47 -0400

    amplify configure

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import { store, persistor } from './redux/store';
 import './index.css';
 import App from './App';
 
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
